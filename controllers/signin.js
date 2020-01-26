a801573c68894d0ff12b74dfa6e5e223dd8ba5e8
camilojimenezf <camilob270@gmail.com>
2020-01-26 14:19:38 -0300

    first commit

---
+++ controllers/signin.js
@@ -0,0 +1,24 @@
+const handleSignin = (db, bcrypt) => (req, res) => {
+    const {email, password} = req.body;
+    if ( !email || !password ) {
+        return res.status(400).json('incorrect from submission');
+    }
+    db.select('email','hash').from('login')
+    .where({email})
+    .then( data => {
+        if( bcrypt.compareSync(password, data[0].hash) ) {
+            db.select('*').from('users').where({email})
+            .then(user => {
+                res.json(user[0]);
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+            res.status(400).json('invalid credentials');
+        }
+    })
+    .catch( err => res.status(400).json('wrong credentials'))
+}
+
+module.exports = {
+    handleSignin
+}
